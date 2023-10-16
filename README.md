7bb16173bccbbe06a5b54c7255be2b2b2955c2ab
delxian <105181034+delxian@users.noreply.github.com>
2023-10-16 13:55:43 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# Image Steganography
+Encodes text within the color data of a PNG image.
+
+## Features
+- Image steganography encoding and decoding
+- Load text to encode from .txt and/or save decoded text to .txt
+- Semi-even data distribution across the image with looping behavior
+- Five demo images from [Pexels](https://www.pexels.com/) (medium size,converted from .jpg to .png)
+- Three sample texts
+    - [Declaration of Independence](https://www.archives.gov/founding-docs/declaration-transcript) - 9KB, ~8.2K characters
+    - [Lorem ipsum, 50 paragraphs](https://www.lipsum.com/) - 29KB, ~29.5K characters
+    - [Romeo and Juliet](https://www.gutenberg.org/ebooks/1513), from [Project Gutenberg](https://www.gutenberg.org/policy/license.html) - 164KB, ~166.9K characters
+        - This eBook is for the use of anyone anywhere in the United States and most other parts of the world at no cost and with almost no restrictions whatsoever. You may copy it, give it away or re-use it under the terms of the Project Gutenberg License included with this eBook or online at www.gutenberg.org. If you are not located in the United States, you will have to check the laws of the country where you are located before using this eBook.
+- Custom cycle cipher with letter-to-parameter conversion
+
+## Steganography
+
+[Steganography](https://en.wikipedia.org/wiki/Steganography) is the act of concealing information within other information. This project uses the color values of pixels in a PNG image to encode ASCII text as binary. Once encoded, the text can be extracted from the image given the interval and offset used to encode it. This project currently uses the LSB (least significant bit) encoding method, which modifies only the least significant bit of each color value, resulting in imperceptible changes to the image.
+
+## Data Distribution
+
+Given the encoded binary data is shorter than the number of color values in the image, binary data is distributed semi-evenly across the image. If the distribution interval extends the encoded data beyond the length of the image data, the overflow data loops back to the beginning exhaustively such that no two data bits overlap. This is accomplished using the modulo operator and its behavior with coprimes. If the encoding interval is coprime with the length of the carrier, it guarantees that no two indices of data bits are equal, even in the worst case scenario where the data length is equal to the carrier length.
+
+For example, if both the data and carrier are 20 entries long, as long as the interval is coprime with 20, there will be no identical indices:
+
+```
+data_length, carrier_length = 20, 20
+indices = list(SteganoImage.distribute(data_length, carrier_length, interval=3))  # coprime
+# [0, 3, 6, 9, 12, 15, 18, 1, 4, 7, 10, 13, 16, 19, 2, 5, 8, 11, 14, 17]
+len(set(indices)) == 20  # True
+indices = list(SteganoImage.distribute(data_length, carrier_length, interval=7))  # coprime
+# [0, 7, 14, 1, 8, 15, 2, 9, 16, 3, 10, 17, 4, 11, 18, 5, 12, 19, 6, 13]
+len(set(indices)) == 20  # True
+indices = list(SteganoImage.distribute(data_length, carrier_length, interval=11))  # coprime
+# [0, 11, 2, 13, 4, 15, 6, 17, 8, 19, 10, 1, 12, 3, 14, 5, 16, 7, 18, 9]
+len(set(indices)) == 20  # True
+indices = list(SteganoImage.distribute(data_length, carrier_length, interval=5))  # not coprime
+# [0, 5, 10, 15, 0, 5, 10, 15, 0, 5, 10, 15, 0, 5, 10, 15, 0, 5, 10, 15]
+len(set(indices)) == 20  # False
+indices = list(SteganoImage.distribute(data_length, carrier_length, interval=6))  # not coprime
+# [0, 6, 12, 18, 4, 10, 16, 2, 8, 14, 0, 6, 12, 18, 4, 10, 16, 2, 8, 14]
+len(set(indices)) == 20  # False
+indices = list(SteganoImage.distribute(data_length, carrier_length, interval=14))  # not coprime
+# [0, 14, 8, 2, 16, 10, 4, 18, 12, 6, 0, 14, 8, 2, 16, 10, 4, 18, 12, 6]
+len(set(indices)) == 20  # False
+```
+
+If two numbers are coprime, their greatest common factor is 1. Thus, the expression `math.gcd(a, b) == 1` can be used to determine if `a` and `b` are coprime.
+
+## Cycle Cipher
+
+This program includes an optional cipher to scramble the data before it is encoded into the image. The cipher employs a basic cycling/rotating method that groups every nth character together and rotates them by some amount. Each rotation operation includes an interval, an offset, and a shift amount.
+
+Demonstration:
+
+```
+text, code = "This is a test sentence.", "213"  # interval = 2, offset = 1, shift = 3
+# every 2nd character, starting at index 1, rotates left by 3 positions
+enciphered = cycle_cipher(text, code)  # "T i  estastnse ce.thnsei"
+deciphered = cycle_cipher(text, code, decode=True)  # "This is a test sentence."
+```
+
+These rotation operations can be chained together to further scramble the data.
+
+While the cycle cipher takes numerical values as parameters, letters are also allowed. The `alpha_to_cipher` function converts each letter in a string into a group of rotation parameters. For example, the word `hello` becomes `414521413413541`, parsed by the cycle cipher as `414`, `521`, `413`, `413`, and `541`. The conversion implementation is mostly arbitrary, and there is no requirement for each letter to have a unique group of parameters.
+
+## Requirements
+- Python 3.11 or higher
+- [Pillow](https://pypi.org/project/Pillow/)
+## License
+- [MIT](LICENSE)
---
+++ main.py
@@ -0,0 +1,241 @@
+"""todo"""
+#pylint: disable=invalid-name
+from collections import deque
+from collections.abc import Iterator
+from functools import cache
+from itertools import chain
+import math
+import os
+import string
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+
+from PIL import Image
+
+
+END_OF_TEXT = "00000011"  # https://www.ascii-code.com/character/%E2%90%83
+
+# images downloaded at medium size and converted to .png in paint
+# https://www.pexels.com/photo/photo-of-buildings-during-nighttime-2603464/  buildings.png
+# https://www.pexels.com/photo/multicolored-abstract-painting-2860810/  abstract.png
+# https://www.pexels.com/photo/colorful-lanterns-hanging-4406455/  lanterns.png
+# https://www.pexels.com/photo/white-clouds-and-blue-sky-3689875/  umbrella.png
+# https://www.pexels.com/photo/stairs-grayscale-photography-921025/  stairs.png
+
+
+def str_to_bin(text: str) -> str:
+    """Convert an ASCII (8-bit) string to a binary string."""
+    return ''.join([bin(ord(i))[2:].zfill(8) for i in text])
+
+def bin_to_str(bits: str) -> str:
+    """Convert a binary string to an ASCII (8-bit) string."""
+    bits = bits[: len(bits) - len(bits) % 8]  # truncate incomplete bytes from end
+    partitioned = [bits[i*8 : i*8 + 8] for i in range(int(len(bits)/8))]
+    return ''.join([chr(int(i, 2)) for i in partitioned])
+
+def cycle_cipher(text: str, code: str, decode: bool = False) -> str:
+    """Encipher/decipher a string with a sequence of rotations of every n characters."""
+    @cache
+    def alpha_to_cycle(text: str) -> str:
+        """Convert letters to codes for the cycle cipher."""
+        def digital_root(n: int) -> int:
+            """Get the ultimate 1-digit sum of a number's digits."""
+            while n // 10:
+                n = sum(int(x) for x in str(n))
+            return n
+
+        if len(text) > 1:
+            return ''.join(alpha_to_cycle(x) for x in text)
+        if (text := text.lower()) not in string.ascii_lowercase:
+            return ''  # non-letter characters do nothing
+
+        # methods for calculating interval/offset/shift are mostly arbitrary
+        # main idea was getting interval/shift relatively low and offset < interval
+        value = ord(text) - 96  # a >> 1, z >> 26
+        interval = int(value)
+        while True:
+            # get largest factor from 2, 3, 4, 5
+            if (factor := next((n for n in range(5, 1, -1) if not interval % n), 0)):
+                interval = factor
+                break
+            # if not possible, get digital root in 2, 3, 4, 5
+            if (root := digital_root(interval)) in range(2, 6):
+                interval = root
+                break
+            # if not possible, incrememnt value and start over
+            interval += 1
+
+        # offset is always < interval and most of the time 0 or 1
+        offset = digital_root(value * digital_root(value)) % interval
+        shift = value % 5 + 1
+        return ''.join(str(n) for n in (interval, offset, shift))
+
+    new = [*text]
+    if not code.isnumeric():
+        code = alpha_to_cycle(code)
+    if len(code) % 3:
+        raise ValueError("Numerical code length must be a multiple of 3")
+    if not all(int(n) for n in code[::3]):
+        raise ValueError("Intervals must be non-zero")
+    group_count = len(code) // 3
+    for n in range(group_count):
+        if decode:
+            n = group_count - (n+1)  # reverse group order
+        interval, offset, shift = [int(x) for x in code[n*3 : n*3 + 3]]
+        chars = deque(new[offset :: interval])
+        chars.rotate(shift if decode else -shift)
+        for i, char in enumerate(chars):
+            new[offset + i*interval] = char
+    return ''.join(new)
+
+def closest_coprimes(dividend: int, num: int) -> tuple[int, int]:
+    """Get the coprimes of a number closest below and above a starting value."""
+    lower, higher = num, num
+    while True:
+        higher += 1
+        if math.gcd(higher, dividend) == 1:
+            break
+    while True:
+        lower -= 1
+        if math.gcd(lower, dividend) == 1:
+            break
+    return (lower, higher)
+
+
+class SteganoImage:
+    """Implement image steganography with 24-bit color."""
+
+    def __init__(self, image: Image.Image):
+        self.image = image
+        raw_image_data = self.image.getdata()
+        if len(raw_image_data[0]) == 4:
+            self.image = self.image.convert('RGB')  # omit transparency data
+            raw_image_data = self.image.getdata()
+        self.image_data = list(chain.from_iterable(raw_image_data))  # collapse to 1D array
+        self.pixel_length = len(raw_image_data)  # pixels in image
+        self.color_length = len(self.image_data)  # color values in image (pixels * 3)
+        assert self.pixel_length*3 == self.color_length, "Image is not 3 colors per pixel"
+
+    def encode(self, text: str, interval: int = 0,
+               offset: int = 0) -> tuple[Image.Image, int, int]:
+        """Encode text into an image."""
+        image = self.image.copy()
+        image_data = list(chain.from_iterable(image.getdata()))
+        text_binary = str_to_bin(text) + END_OF_TEXT  # end of text character
+        if len(text_binary) > self.color_length:
+            raise ValueError("Text too long for carrier image")
+        interval = interval if interval else self.color_length // len(text_binary)
+        indices = SteganoImage.distribute(
+            len(text_binary), self.color_length, interval, offset)
+        for bit, index in zip(text_binary, indices):
+            color = image_data[index]
+            image_data[index] = self.modify_color(color, int(bit), "LSB")
+            pixel_index = index // 3
+            y, x = divmod(pixel_index, image.width)
+            rgb = tuple(image_data[pixel_index*3 : pixel_index*3+3])
+            image.putpixel((x, y), rgb)  # type: ignore
+        return (image, interval, offset)
+
+    @staticmethod
+    def modify_color(color: int, data: int, mode: str) -> int:
+        """Modify the value of a single color in an image to encode data."""
+        if mode == "LSB":  # 1 data bit to 1 color bit
+            if color not in range(256):
+                raise ValueError("Invalid 8-bit color value")
+            if data.bit_length() > 1:
+                raise ValueError("Too much data bits for LSB mode")
+            return (color & ~1) + data
+        return color
+
+    @staticmethod
+    def decode(image: Image.Image, interval: int, offset: int = 0) -> str:
+        """Extract text from a pre-encoded image."""
+        image = image.convert('RGB')  # omit transparency data
+        image_data = list(chain.from_iterable(image.getdata()))
+        indices = SteganoImage.distribute(
+            len(image_data), len(image_data), interval, offset)
+        reconstructed_binary: str = ''
+        for index in indices:
+            reconstructed_binary += str(image_data[index] % 2)
+            if not len(reconstructed_binary) % 8:
+                # stop reading at end of text character
+                if reconstructed_binary[-8:] == END_OF_TEXT:
+                    break
+        return bin_to_str(reconstructed_binary)
+
+    @staticmethod
+    def distribute(data_length: int, carrier_length: int,
+                   interval: int = 0, offset: int = 0) -> Iterator[int]:
+        """
+        Generate a semi-even distribution of indices across a range. \\
+        Supports looping behavior where indices beyond the carrier length \\
+        loop back to the start with no overlap, even in the worst case scenario. \\
+        For this to occur, the interval must be coprime with the carrier length.
+        """
+        if data_length > carrier_length:
+            raise ValueError("Insufficient space")
+        interval = interval if interval else carrier_length // data_length
+        if math.gcd(interval, carrier_length) != 1:  # coprimes guarantee no overlap
+            interval = closest_coprimes(carrier_length, interval)[1]
+        if interval > carrier_length:
+            raise RuntimeError("Interval too high")  # technically not a problem
+        offset %= interval
+        return ((n*interval + offset) % carrier_length for n in range(data_length))
+
+
+image_filepath = askopenfilename(
+    title="Select image", filetypes=[("Image files", "*.png")])
+my_image = Image.open(image_filepath)
+print(f"Loaded image {os.path.basename(image_filepath)}.")
+print(f"Image dimensions: {my_image.size}")
+if (encode := input("Encode or decode message? (e/d): ")) == 'e':
+    stegano_image = SteganoImage(my_image)
+    print(f"Maximum LSB encoding: {stegano_image.color_length} bits, " \
+          f"{stegano_image.color_length // 8} characters")
+    if not (encode_text := input("Enter text to encode (leave blank to choose .txt file): ")):
+        text_filepath = askopenfilename(
+            title="Select text file", filetypes=[("Text files", "*.txt")])
+        with open(text_filepath, mode='r', encoding="UTF-8") as file:
+            encode_text = file.read()
+        encode_text = ''.join(filter(lambda x: x in string.printable, encode_text))
+        print(f"Loaded text {os.path.basename(text_filepath)}.")
+    print(f"Text length: {len(encode_text)} characters")
+    if input("Encipher text using cycle cipher? (y/n): ") == 'y':
+        print("Rotation parameters follow an IOS format - Interval, Offset, Shift")
+        cipher_code = input("Enter code for cycle cipher " \
+                            "(letters or groups of 3 numbers [e.g. apple, 314202]): ")
+        encode_text = cycle_cipher(encode_text, cipher_code)
+    if input("Show text to encode? (y/n): ") == 'y':
+        print(f"Text to encode: {encode_text}")
+    encoding_interval = input(
+        "Enter custom distribution interval (leave blank for even distribution): ")
+    encoding_interval = int(encoding_interval) if encoding_interval else 0
+    encoding_offset = input("Enter custom distribution offset (leave blank for 0): ")
+    encoding_offset = int(encoding_offset) if encoding_offset else 0
+    encoded, encoding_interval, encoding_offset = stegano_image.encode(
+        encode_text, interval=encoding_interval, offset=encoding_offset)
+    if input("Save encoded image? (y/n): ") == 'y':
+        output_image_filepath = asksaveasfilename(
+            title="Save image", filetypes=[("Image files", "*.png")])
+        encoded = encoded.convert("RGB")
+        encoded.save(output_image_filepath, format="PNG", quality=100)
+        print(f"Image saved to {output_image_filepath}.")
+        print(f"Encoding interval: {encoding_interval}")
+        print(f"Encoding offset: {encoding_offset}")
+        print("Remember these values; you cannot decode the text without them.")
+else:
+    decoding_interval = int(input("Enter interval for decoding: "))
+    decoding_offset = int(input("Enter offset for decoding: "))
+    decoded_text = SteganoImage.decode(my_image, decoding_interval, decoding_offset)
+    if input("Decipher text using cycle cipher? (y/n): ") == 'y':
+        cipher_code = input("Enter code for cycle cipher " \
+                            "(string or groups of 3 numbers [e.g. apple, 314202]): ")
+        decoded_text = cycle_cipher(decoded_text, cipher_code, decode=True)
+    if input("Show decoded text? (y/n): ") == 'y':
+        print(f"Decoded text: {decoded_text}")
+    print(f"Text length: {len(decoded_text)} characters")
+    if input("Save decoded text to .txt? (y/n): ") == 'y':
+        output_text_filepath = asksaveasfilename(
+            title="Save text", filetypes=[("Text files", "*.txt")])
+        with open(output_text_filepath, mode='w', encoding="UTF-8") as file:
+            file.write(decoded_text)
+        print(f"Text saved to {output_text_filepath}.")
